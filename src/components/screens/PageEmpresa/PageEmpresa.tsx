c353ee33177fbe3246ef6f14058589bcffbd7c7a
Julian Gonzalez Endzweig <gonzalezjulian501@gmail.com>
2024-10-28 01:23:20 -0300

    Added "Vista Empresa " display changes to improve component dynamics

--- src/components/screens/PageEmpresa/PageEmpresa.tsx
+++ src/components/screens/PageEmpresa/PageEmpresa.tsx
@@ -1,15 +1,18 @@
 import { Header } from "../../ui/Header/Header.tsx";
-import { CardEmpresa } from "../../ui/CardEmpresa/CardEmpresa.tsx";
-import { CardSucursal } from "../../ui/CardSucursal/CardSucursal.tsx";
+// import { CardEmpresa } from "../../ui/CardEmpresa/CardEmpresa.tsx";
+import {  PageSucursal } from "../../ui/PageSucursal/PageSucursal.tsx";
 import "./PageEmpresa.css";
+import { MenuEmpresa } from "../../ui/MenuEmpresa/MenuEmpresa.tsx";
+// import { CardEmpresa } from "../../ui/CardEmpresa/CardEmpresa.tsx";
 
 export const PageEmpresa = () => {
   return (
     <div className="pageEmpresaContainer">
       <Header nombreVista="Vista empresas" />
       <div className="pageEmpresaSucursal">
-        <CardEmpresa />
-        <CardSucursal />
+        {/* <CardEmpresa /> */}
+        <MenuEmpresa/>
+        <PageSucursal />
       </div>
     </div>
   );
